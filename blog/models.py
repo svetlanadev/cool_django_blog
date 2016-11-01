b0c6c329efa1de099785b942e9d02b2df5896775
Svetik <turistenok1@gmail.com>
2016-11-01 15:33:42 +0200

    blog settings and structure

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+
+class Post(models.Model):
+    title = models.CharField(max_length=94)
+    # author = models.ForeignKey(User)
+    created = models.DateTimeField(auto_now_add=True)
+    body = models.TextField()
+    views = models.IntegerField(default=0)
+    url = models.URLField(blank=True)
+
